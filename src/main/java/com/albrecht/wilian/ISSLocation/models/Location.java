f4d83d6d3011f1aea9ff60a60c02d98df78ea51d
wilianAlbrecht <wilian0012@hotmail.com>
2023-10-03 19:57:18 -0400

    desenvolvido end-point que busca a posição da iss e retorna em json

--- src/main/java/com/albrecht/wilian/ISSLocation/models/Location.java
+++ src/main/java/com/albrecht/wilian/ISSLocation/models/Location.java
@@ -2,4 +2,28 @@ package com.albrecht.wilian.ISSLocation.models;
 
 public class Location {
 	
+	private String latitude;
+	private String longitude;
+	
+	
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+	public Location(String latitude, String longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
 }
--- src/main/java/com/albrecht/wilian/ISSLocation/resources/LocationResource.java
+++ src/main/java/com/albrecht/wilian/ISSLocation/resources/LocationResource.java
@@ -4,17 +4,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.albrecht.wilian.ISSLocation.models.Location;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @RestController
 @RequestMapping("/api")
 public class LocationResource {
 	
+	private RestTemplate restTemplate;
+	
 	@GetMapping("iss-location")
 	public ResponseEntity issLocation() {
 		
+		restTemplate = new RestTemplate();
 		
 		
-		return ResponseEntity.ok("");
+		JsonObject jsonObject = JsonParser.parseString(restTemplate.getForObject("http://api.open-notify.org/iss-now.json", String.class)).getAsJsonObject();
+		
+		JsonObject issPosition = jsonObject.getAsJsonObject("iss_position");
+		
+		Location location = new Location(issPosition.get("latitude").getAsString(), issPosition.get("longitude").getAsString());
+		
+		return ResponseEntity.ok(location);
 	}
 	
 }
